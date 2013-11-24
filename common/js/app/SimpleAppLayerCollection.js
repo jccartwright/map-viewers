ee27545ae0901fbfb4e4ccb16bef326826bf8745
John Cartwright <john.c.cartwright@noaa.gov>
2013-11-24 09:50:23 -0700

    add some additional layers to support BasemapToolbar testing

--- common/js/app/SimpleAppLayerCollection.js
+++ common/js/app/SimpleAppLayerCollection.js
@@ -34,6 +34,17 @@ define(["dojo/_base/declare", "ngdc/layers/AbstractLayerCollection", "esri/layer
                     new ArcGISDynamicMapServiceLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/SampleWorldCities/MapServer", {
                         id: "cities",
                         visible: true
+                    }),
+                    new ArcGISTiledMapServiceLayer("http://services.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer", {
+                        id: "World Boundaries and Places",
+                        visible: false,
+                        opacity: 1
+                    }),
+                    new ArcGISDynamicMapServiceLayer("http://maps.ngdc.noaa.gov/rest/services/web_mercator/graticule/MapServer", {
+                        id: "Graticule",
+                        visible: false,
+                        opacity: 0.7,
+                        imageParameters: this.imageParameters.png32
                     })
                 ];
             },  //end defineMapServices
