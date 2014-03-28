93b60b86825d13c3cda8f1e7b6f1c1f4951e8e96
John Cartwright <john.c.cartwright@noaa.gov>
2014-03-28 15:18:37 -0600

    add projection-specific placeholder subclasses

---
+++ common/js/ngdc/antarctic/MapConfig.js
@@ -0,0 +1,15 @@
+define(["dojo/_base/declare","ngdc/MapConfig"],
+    function(declare, MapConfig ){
+        return declare([MapConfig], {
+
+            constructor: function(arguments) {
+                logger.debug('inside constructor for ngdc/antarctic/MapConfig');
+            },
+
+            //override method in parent class for projection-specific conversion
+            mapPointToGeographic: function mapPointToGeographic(mapPoint) {
+                //return (webMercatorUtils.webMercatorToGeographic(mapPoint));
+            }
+        });
+    }
+);
