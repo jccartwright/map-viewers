ad18a420cd3f70bb7ec0b74a6b756a38b08acc98
John Cartwright <john.c.cartwright@noaa.gov>
2014-05-05 16:51:33 -0600

    replace version 1.x of bathymetry with bathymetry-2.0 branch

---
+++ bathymetry/js/app/AppIdentifyPane.js
@@ -0,0 +1,159 @@
+define(["dojo/_base/declare", "dojo/_base/array", "dojo/string", "ngdc/identify/IdentifyPane", "dojo/topic", "esri/dijit/Popup", "dojo/_base/lang", "dijit/form/Button",
+    "dojo/dom-style", "app/RequestDataDialog"],
+    function(declare, array, string, IdentifyPane, topic, Popup, lang, Button,
+             domStyle, RequestDataDialog){
+
+        return declare([IdentifyPane], {
+
+            postCreate: function() {
+                this.inherited(arguments);
+                domStyle.set(this.domNode, 'height', '350px');
+                domStyle.set(this.domNode, 'width', '400px');
+
+                domStyle.set(this.featurePageBottomBar.domNode, 'height', '30px');
+                //this.featurePageBottomBar.style = 'height: 50px;';
+
+                this.requestDataFilesButton = new Button({
+                    label: "Request These Data Files",
+                    style: "bottom: 5px; left: 15px;",
+                    onClick: lang.hitch(this, function(){
+                        this.requestDataFiles();
+                    })
+                }).placeAt(this.featurePageBottomBar);
+
+                this.requestDataFileButton = new Button({
+                    label: "Request This Data File",
+                    style: "bottom: 25px; left: 15px;",
+                    onClick: lang.hitch(this, function(){
+                        this.requestDataFile();
+                    })
+                }).placeAt(this.infoPageBottomBar);
+            },
+
+            showResults: function(resultCollection) {
+                this.inherited(arguments);
+                if (this.numFeatures >= 1000) {
+                    this.featurePageTitle = "Identified Features (" + this.numFeatures + "+, results limited to 1000)";
+                    this.setTitle(this.featurePageTitle);
+                }
+            },
+
+            getLayerDisplayLabel: function(item) {
+                return '<i><b>' + item.layerName + '</b></i>';
+            },
+
+            getItemDisplayLabel: function(item) {
+                return item.value;
+            },
+
+            requestDataFiles: function() {
+                var items = this.storeModel.store.query({type: 'item'});
+                var fileInfos = [];
+                for (var i = 0; i < items.length; i++) {
+                    fileInfos.push({
+                        file: items[i].displayLabel,
+                        cruise: items[i].attributes['Survey ID'],
+                        ship: items[i].attributes['Ship Name'],
+                        instrument: items[i].attributes['Instrument Name']
+                    });
+                }
+
+                if (!this.requestDataDialog) {
+                    this.requestDataDialog = new RequestDataDialog({style: 'width: 300px;'});
+                }
+                this.requestDataDialog.fileInfos = fileInfos;
+                this.requestDataDialog.show();
+            },
+
+            requestDataFile: function() {
+                var fileInfo = {
+                    file: this.currentItem.displayLabel,
+                    cruise: this.currentItem.attributes['Survey ID'],
+                    ship: this.currentItem.attributes['Ship Name'],
+                    instrument: this.currentItem.attributes['Instrument Name']
+                };
+
+                if (!this.requestDataDialog) {
+                    this.requestDataDialog = new RequestDataDialog({style: 'width: 300px;'});
+                }
+                this.requestDataDialog.fileInfos = [fileInfo];
+                this.requestDataDialog.show();
+            },
+
+            populateFeatureStore: function(results) {
+                var numFeatures = 0;
+                this.uid = 0;
+                this.expandedNodePaths = [];
+                for (var svcName in results) {
+                    for (var layerName in results[svcName]) {
+
+                        numFeatures += results[svcName][layerName].length;
+                        for (var i = 0; i < results[svcName][layerName].length; i++) {
+                            var item = results[svcName][layerName][i];
+                            var layerKey = svcName + '/' + layerName;
+                            var layerUrl = results[svcName][layerName][i].layerUrl;
+                            //Create a layer "folder" node if it doesn't already exist
+                            if (this.featureStore.query({id: layerName}).length == 0) {
+                                this.featureStore.put({
+                                    uid: ++this.uid,
+                                    id: layerName,
+                                    label: this.getLayerDisplayLabel(item),
+                                    type: 'folder',
+                                    parent: 'root'
+                                });
+                            }
+                            //Create a survey "folder" node if it doesn't already exist
+                            var surveyId = item.feature.attributes['Survey ID'];
+                            var surveyKey = layerName + '/' + surveyId;
+                            if (this.featureStore.query({id: surveyKey}).length == 0) {
+                                this.featureStore.put({
+                                    uid: ++this.uid,
+                                    id: surveyKey,
+                                    label: '<b>Survey: ' + surveyId + '</b>',
+                                    type: 'folder',
+                                    parent: layerName
+                                });
+                            }
+                            //Create an instrument "folder" node if it doesn't already exist
+                            var instrument = item.feature.attributes['Instrument Name'];
+                            var instrumentKey = layerName + '/' + surveyId + '/' + instrument;
+                            if (this.featureStore.query({id: instrumentKey}).length == 0) {
+                                this.featureStore.put({
+                                    uid: ++this.uid,
+                                    id: instrumentKey,
+                                    label: '<b>Instrument: ' + instrument + '</b>',
+                                    type: 'folder',
+                                    parent: surveyKey
+                                });
+
+                                //Add this node to the list of nodes to be expanded to in constructFeatureTree
+                                this.expandedNodePaths.push(['root', layerName, surveyKey, instrumentKey]);
+                            }
+
+                            //Add the current item to the store, with the layerName as parent
+                            this.featureStore.put({
+                                uid: ++this.uid,
+                                id: this.uid,
+                                //TODO: point to the magnifying glass image using a module path
+                                displayLabel: this.getItemDisplayLabel(item),
+                                label: this.getItemDisplayLabel(item) + " <a id='zoom-" + this.uid + "' href='#' class='zoomto-link'><img src='js/ngdc/identify/images/magnifying-glass.png'></a>",
+                                layerUrl: layerUrl,
+                                layerKey: layerKey,
+                                attributes: item.feature.attributes,
+                                parent: instrumentKey,
+                                type: 'item'
+                            });
+                        }
+                    }
+                }
+                return numFeatures;
+            },
+
+            constructFeatureTree: function() {
+                this.inherited(arguments);
+                //Expand the tree to the instrument level. All nodes will be opened except for these.
+                this.tree.set('paths', this.expandedNodePaths);
+            }
+        });
+    }
+);
---
+++ bathymetry/js/app/AppLoader.js
@@ -0,0 +1,163 @@
+define([
+    "dojo/_base/declare",
+    "dijit/registry",
+    "dojo/dom",
+    "dojo/_base/config",
+    "dojo/io-query",
+    "dojo/_base/lang",
+    "dojo/topic",
+    "dojo/on",
+    "dojo/aspect",
+    "esri/config",
+    "esri/geometry/Extent",
+    "esri/SpatialReference",
+    "ngdc/Logger",
+    "ngdc/web_mercator/MapConfig",
+    "ngdc/arctic/MapConfig",
+    "ngdc/antarctic/MapConfig",
+    "ngdc/Banner",
+    "app/MercatorLayerCollection",
+    "app/ArcticLayerCollection",
+    "app/AntarcticLayerCollection",
+    "dojo/domReady!"],
+    function(
+        declare,
+        registry,
+        dom,
+        config,
+        ioQuery,
+        lang,
+        topic,
+        on,
+        aspect,
+        esriConfig,
+        Extent,
+        SpatialReference,
+        Logger,
+        MercatorMapConfig,
+        ArcticMapConfig,
+        AntarcticMapConfig,
+        Banner,
+        MercatorLayerCollection,
+        ArcticLayerCollection,
+        AntarcticLayerCollection
+        ){
+        return declare(null, {
+            constructor:function(args){
+                declare.safeMixin(this,args);
+                this.overlayNode = dom.byId(this.overlayNodeId);
+            },
+
+            init: function() {
+                esriConfig.defaults.io.corsEnabledServers = [
+                    "http://maps.ngdc.noaa.gov/arcgis/rest/services",
+                    "http://agsdevel.ngdc.noaa.gov/arcgis/rest/services"];
+
+                //add queryParams into config object, values in queryParams take precedence
+                var queryParams = ioQuery.queryToObject(location.search.substring(1));
+                lang.mixin(config.app, queryParams);
+
+                //put the logger into global so all modules have access
+                window.logger = new Logger(config.app.loglevel);
+
+                this.setupBanner();
+
+                this.setupMapViews();
+            },
+
+            setupBanner: function() {
+                var banner = new Banner({
+                    breadcrumbs: [
+                        {url: 'http://www.noaa.gov', label: 'NOAA'},
+                        {url: 'http://www.nesdis.noaa.gov', label: 'NESDIS'},
+                        {url: 'http://www.ngdc.noaa.gov', label: 'NGDC'},
+                        {url: 'http://maps.ngdc.noaa.gov/viewers', label: 'Maps'},
+                        {url: 'http://www.ngdc.noaa.gov/mgg/bathymetry/relief.html', label: 'Bathymetry'}
+                    ],
+                    dataUrl: "http://www.ngdc.noaa.gov/mgg/bathymetry/relief.html",
+                    image: "images/bathymetry_viewer_logo.png"
+                });
+                banner.placeAt('banner');
+            },
+
+            setupMapViews: function() {
+                logger.debug('setting up map views...');
+                // setup map views. You can only draw a Map into a visible container
+                this.setupMercatorView();
+
+                registry.byId('mapContainer').selectChild('arctic');
+                this.setupArcticView();
+
+                registry.byId('mapContainer').selectChild('antarctic');
+                this.setupAntarcticView();
+
+                //go back to mercator as default view
+                registry.byId('mapContainer').selectChild('mercator');
+            },
+
+            setupMercatorView: function() {
+                logger.debug('setting up Mercator view...');
+                var mapConfig = new MercatorMapConfig("mercator", {
+                    center:[-45,45],
+                    zoom: 3,
+                    logo: false,
+                    showAttribution: false,
+                    overview: true,
+                    sliderStyle: 'large'
+                }, new MercatorLayerCollection());
+
+                //var mapToolbar = new MapToolbar({map: mapConfig.map}, "mercatorMapToolbar");
+
+                //var basemapToolbar = new BasemapToolbar({layerCollection: mapConfig.mapLayerCollection}, "mercatorBasemapToolbar");
+                //basemapToolbar.startup();
+
+                //var coordinatesToolbar = new CoordinatesToolbar({map: mapConfig.map}, "mercatorCoordinatesToolbar");
+
+            },
+
+            setupArcticView: function() {
+                logger.debug('setting up Arctic view...');
+                /*
+                var initialExtent = new Extent({
+                    xmin:-30000000,
+                    ymin:-30000000,
+                    xmax:30000000,
+                    ymax:30000000,
+                    spatialReference: new SpatialReference({wkid:3995})
+                });
+                */
+
+                var mapConfig = new ArcticMapConfig("arctic", {
+                    //extent: initialExtent,
+                    //center:[0, 89],
+                    zoom: 2,
+                    logo: false,
+                    showAttribution: false,
+                    overview: false,
+                    sliderStyle: 'large'
+                }, new ArcticLayerCollection());
+            },
+
+            setupAntarcticView: function() {
+                logger.debug('setting up Antarctic view...');
+                /*
+                var initialExtent = new Extent({
+                    xmin:-30000000,
+                    ymin:-30000000,
+                    xmax:30000000,
+                    ymax:30000000,
+                    spatialReference: new SpatialReference({wkid:3031})
+                });
+                */
+
+                var mapConfig = new AntarcticMapConfig("antarctic", {
+                    zoom: 3,
+                    logo: false,
+                    showAttribution: false,
+                    overview: false,
+                    sliderStyle: 'large'
+                }, new AntarcticLayerCollection());
+            }
+        })
+    }
+);
