cf5d6b7e37a7e1c0dc9d131d85c2827ebbd3463b
John Cartwright <john.c.cartwright@noaa.gov>
2014-05-06 14:12:02 -0600

    add basemap toolbar for mercator view on bathy

--- bathymetry/js/app/BasemapToolbar.js
+++ bathymetry/js/app/BasemapToolbar.js
@@ -6,8 +6,8 @@ define(["dojo/_base/declare", "ngdc/BasemapToolbar"],
                 //TODO redesign this data structure
                 this._basemaps = [
                     {services: ['Ocean Basemap'], label: 'Ocean Basemap (Esri)', boundariesEnabled: false},
-                    {services: ['GEBCO_08'], label: 'Shaded Relief (GEBCO_08)', boundariesEnabled: true},
-                    {services: ['ETOPO1'], label: 'Shaded Relief (ETOPO1)', boundariesEnabled: true},
+                    //{services: ['GEBCO_08'], label: 'Shaded Relief (GEBCO_08)', boundariesEnabled: true},
+                    //{services: ['ETOPO1'], label: 'Shaded Relief (ETOPO1)', boundariesEnabled: true},
                     {services: ['Light Gray', 'Light Gray Reference'], label: 'Light Gray (Esri)', boundariesEnabled: false},
                     {services: ['World Imagery'], label: 'World Imagery (Esri)', boundariesEnabled: true},
                     {services: ['NatGeo Overview'], label: 'National Geographic (Esri)', boundariesEnabled: false}
