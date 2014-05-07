90c93c2a7d1618c2bb8a8d04d753b2e6ab6926ba
jvarner <jesse.varner@noaa.gov>
2014-05-07 17:17:07 -0600

    Utilize new identify menu in the BasemapToolbar

--- water_column_sonar/js/app/BasemapToolbar.js
+++ water_column_sonar/js/app/BasemapToolbar.js
@@ -19,6 +19,13 @@ define(["dojo/_base/declare", "ngdc/BasemapToolbar"],
                     {services: ['Graticule'], label: 'Graticule'}
                 ];
 
+                this._identifyTools = [
+                    {label: 'Point (Single-Click)', id: 'point', iconClass: 'identifyByPointIcon'},
+                    {label: 'Draw Rectangle', id: 'rect', iconClass: 'identifyByRectIcon'},
+                    {label: 'Draw Polygon', id: 'freehand-polygon', iconClass: 'identifyByPolygonIcon'},
+                    {label: 'Define Bounding Box', id: 'coords', iconClass: 'identifyByCoordsIcon'}
+                ];
+
                 //define the default base
                 this.defaultBasemapIndex = 0;
 
