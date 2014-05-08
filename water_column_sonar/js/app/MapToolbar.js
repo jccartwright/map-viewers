df6059ff6ed78184b563f39d57d6624978afbffd
jvarner <jesse.varner@noaa.gov>
2014-05-08 17:40:53 -0600

    New data structure for basemaps/overlays: each basemap "base" has its own toggleable overlay(s). Other small code cleanup/fixes.

--- water_column_sonar/js/app/MapToolbar.js
+++ water_column_sonar/js/app/MapToolbar.js
@@ -3,19 +3,18 @@ define(["dojo/_base/declare", "ngdc/MapToolbar"],
 
         return declare([MapToolbar], {
             constructor: function() {
-                //TODO redesign this data structure
-                this._basemaps = [
-                    {services: ['Ocean Base', 'Ocean Reference'], label: 'Ocean Basemap (Esri)', boundariesEnabled: false},
 
-                    {services: ['GEBCO_08'], label: 'Shaded Relief (GEBCO_08)', boundariesEnabled: true},
-                    {services: ['ETOPO1'], label: 'Shaded Relief (ETOPO1)', boundariesEnabled: true},
-                    {services: ['Light Gray', 'Light Gray Reference'], label: 'Light Gray (Esri)', boundariesEnabled: false},
-                    {services: ['World Imagery'], label: 'World Imagery (Esri)', boundariesEnabled: true},
-                    {services: ['NatGeo Overview'], label: 'National Geographic (Esri)', boundariesEnabled: false}
+                this._basemaps = [
+                    {base: 'Ocean Base', overlays: ['Ocean Reference'], label: 'Ocean Basemap (Esri)'},
+                    {base: 'GEBCO_08', overlays: ['World Boundaries and Places'], label: 'Shaded Relief (GEBCO_08)'},
+                    {base: 'ETOPO1', overlays: ['World Boundaries and Places'], label: 'Shaded Relief (ETOPO1)'},
+                    {base: 'Light Gray', overlays: ['Light Gray Reference'], label: 'Light Gray (Esri)'},
+                    {base: 'World Imagery', overlays: ['World Boundaries and Places'], label: 'World Imagery (Esri)'},
+                    {base: 'NatGeo', label: 'National Geographic (Esri)'} //NatGeo has no boundaries overlay
                 ];
 
                 this._overlays = [
-                    {services: ['World Boundaries and Places'], label: 'Boundaries/Labels'},
+                    {services: ['Ocean Reference'], label: 'Boundaries/Labels'}, //Ocean Basemap/Reference is the default combo
                     {services: ['Graticule'], label: 'Graticule'}
                 ];
 
