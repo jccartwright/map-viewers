e0e123111696b55cbb01dd2990bc0d9a7665bb20
jvarner <jesse.varner@noaa.gov>
2014-05-19 18:02:24 -0600

    Add custom lods for Arctic/Antarctic views. Levels 0 and 1 are disabled.

--- bathymetry/js/app/AppLoader.js
+++ bathymetry/js/app/AppLoader.js
@@ -145,7 +145,20 @@ define([
                     xmax: 4000000,
                     ymax: 4000000,
                     spatialReference: new SpatialReference({wkid: 3995})
-                });                
+                });   
+
+                //Arctic/Antarctic enabled zoom levels. Level 0 and 1 are disabled.
+                var lods = [ 
+                //{"level": 0, "resolution": 67733.46880027094, "scale": 256000000}, 
+                //{"level": 1, "resolution": 33866.73440013547, "scale": 128000000}, 
+                {"level": 2, "resolution": 16933.367200067736, "scale": 64000000}, 
+                {"level": 3, "resolution": 8466.683600033868, "scale": 32000000}, 
+                {"level": 4,"resolution": 4233.341800016934,"scale": 16000000}, 
+                {"level": 5,"resolution": 2116.670900008467,"scale": 8000000}, 
+                {"level": 6,"resolution": 1058.3354500042335,"scale": 4000000}, 
+                {"level": 7,"resolution": 529.1677250021168,"scale": 2000000}, 
+                {"level": 8,"resolution": 264.5838625010584,"scale": 1000000} 
+                ];             
 
                 var mapConfig = new ArcticMapConfig("arctic", {
                     extent: initialExtent,
@@ -153,7 +166,8 @@ define([
                     logo: false,
                     showAttribution: false,
                     overview: false,
-                    sliderStyle: 'large'
+                    sliderStyle: 'large',
+                    lods: lods
                 }, new ArcticLayerCollection());
 
                 var mapToolbar = new ArcticMapToolbar({map: mapConfig.map, layerCollection: mapConfig.mapLayerCollection}, "arcticMapToolbar");
@@ -169,7 +183,20 @@ define([
                     xmax: 4000000,
                     ymax: 4000000,
                     spatialReference: new SpatialReference({wkid: 3031})
-                });                
+                });  
+
+                //Arctic/Antarctic enabled zoom levels. Level 0 and 1 are disabled.
+                var lods = [ 
+                //{"level": 0, "resolution": 67733.46880027094, "scale": 256000000}, 
+                //{"level": 1, "resolution": 33866.73440013547, "scale": 128000000}, 
+                {"level": 2, "resolution": 16933.367200067736, "scale": 64000000}, 
+                {"level": 3, "resolution": 8466.683600033868, "scale": 32000000}, 
+                {"level": 4,"resolution": 4233.341800016934,"scale": 16000000}, 
+                {"level": 5,"resolution": 2116.670900008467,"scale": 8000000}, 
+                {"level": 6,"resolution": 1058.3354500042335,"scale": 4000000}, 
+                {"level": 7,"resolution": 529.1677250021168,"scale": 2000000}, 
+                {"level": 8,"resolution": 264.5838625010584,"scale": 1000000} 
+                ];               
 
                 var mapConfig = new AntarcticMapConfig("antarctic", {
                     extent: initialExtent,
@@ -177,7 +204,8 @@ define([
                     logo: false,
                     showAttribution: false,
                     overview: false,
-                    sliderStyle: 'large'
+                    sliderStyle: 'large',
+                    lods: lods
                 }, new AntarcticLayerCollection());
 
                 var mapToolbar = new AntarcticMapToolbar({map: mapConfig.map, layerCollection: mapConfig.mapLayerCollection}, "antarcticMapToolbar");
