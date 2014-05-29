5f501952d3880f0d09626e20b148416ef6e030d6
jvarner <jesse.varner@noaa.gov>
2014-05-29 10:29:08 -0600

    Add the RNC overlay.

--- bathymetry/js/app/web_mercator/MapToolbar.js
+++ bathymetry/js/app/web_mercator/MapToolbar.js
@@ -15,6 +15,7 @@ define(["dojo/_base/declare", "ngdc/MapToolbar"],
                 this._overlays = [
                     {services: ['Ocean Reference'], label: 'Boundaries/Labels'}, //Ocean Basemap/Reference is the default combo
                     {services: ['GEBCO_08 Contours'], label: 'Bathymetry Contours (GEBCO_08)'},
+                    {services: ['RNC'], label: '<a href="http://www.nauticalcharts.noaa.gov/mcd/Raster/" target="_blank">NOAA Raster Navigational Charts (RNC®)</a>'},
                     {services: ['Graticule'], label: 'Graticule'}
                 ];
 
