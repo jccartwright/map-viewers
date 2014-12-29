858d590e07848e83a831f6277fa192e70aa26555
jvarner <jesse.varner@noaa.gov>
2014-12-29 11:26:40 -0700

    Revamped historical_declination viewer to use latest map framework, ArcGIS API 3.12. Added logic to prevent rapid-fire requests from being issued when holding down the arrow keys on the time slider. Use minScale for visibility in 1-degree FeatureLayer. Added Esri Dark Gray basemap.

---
+++ historical_declination/js/app/antarctic/MapToolbar.js
@@ -0,0 +1,35 @@
+define([
+    'dojo/_base/declare', 
+    'ngdc/MapToolbar',
+    'dojo/text!../templates/MapToolbar.html' //Override standard MapToolbar template to remove the identify menu
+    ],
+    function(
+        declare, 
+        MapToolbar,
+        template
+        ){
+
+        return declare([MapToolbar], {
+            templateString: template,
+
+            constructor: function() {
+                this._basemaps = [
+                    {base: 'Antarctic Basemap', overlays: ['Reference'], label: 'Antarctic Basemap (IBCSO/GEBCO_08)'}
+                ];
+
+                this._overlays = [
+                    {services: ['Reference'], label: 'Boundaries/Labels'},
+                    {services: ['Graticule'], label: 'Graticule'}
+                ];
+
+                //define the default base
+                this.defaultBasemapIndex = 0;
+
+                this._validateLayerIds();
+
+            } //end constructor            
+        });
+    }
+);
+
+
