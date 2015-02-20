f4b7f8c04a6924b977c072f6d9f8c7ee8e1285f7
jvarner <jesse.varner@noaa.gov>
2015-02-19 18:07:36 -0700

    Removed "Coastline" layer. "Distance Constraint" is now in the Boundaries group in the map service, but is still categorized with the other Constraint Line Products in the TOC. Updated layer indices.

--- ecs_catalog/js/app/AppLoader.js
+++ ecs_catalog/js/app/AppLoader.js
@@ -62,10 +62,10 @@ define([
             mercatorMapConfig: null,
             arcticMapConfig: null,
 
-            scenarioProductLayerIds: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
-            boundaryLayerIds: [16, 17, 18, 19, 20],
-            sourceDataLayerIds: [23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41],
-            dataProductLayerIds: [43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59],
+            scenarioProductLayerIds: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+            boundaryLayerIds: [15, 16, 17, 18, 19],
+            sourceDataLayerIds: [22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40],
+            dataProductLayerIds: [42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58],
 
             regionFilter: '',
             phaseFilter: '',
