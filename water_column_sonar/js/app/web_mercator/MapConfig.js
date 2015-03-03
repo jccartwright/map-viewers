39988185281a74e9d42bf885745a07d5d6f5692b
jvarner <jesse.varner@noaa.gov>
2015-03-03 13:20:52 -0700

    Configure the MapToolbar with min/max latitude for the BoundingBoxDialog. On arctic view, whenever the layer mode changes between 'cruise' and 'file', close the IdentifyPane to reduce confusion.

--- water_column_sonar/js/app/web_mercator/MapConfig.js
+++ water_column_sonar/js/app/web_mercator/MapConfig.js
@@ -31,8 +31,13 @@ define([
 
                 console.log('inside custom Web Mercator mapReady...');   
 
-                var mapToolbar = new MapToolbar({map: this.map, layerCollection: this.mapLayerCollection}, 'mercatorMapToolbar');
-                mapToolbar.startup();
+                this.mapToolbar = new MapToolbar({
+                    map: this.map, 
+                    layerCollection: this.mapLayerCollection, 
+                    maxLat: 85, 
+                    minLat: -85
+                }, 'arcticMapToolbar');
+                this.mapToolbar.startup();
 
                 this.identify = new Identify({map: this.map, layerCollection: this.mapLayerCollection});
 
