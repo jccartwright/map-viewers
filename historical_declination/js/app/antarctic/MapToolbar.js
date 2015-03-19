9e76e5ac58cc5dd47a522c2bf15cc2c16bdb785c
jvarner <jesse.varner@noaa.gov>
2015-03-19 11:32:13 -0600

    Updated MapToolbar according to dijits-2.3 requirements.

--- historical_declination/js/app/antarctic/MapToolbar.js
+++ historical_declination/js/app/antarctic/MapToolbar.js
@@ -14,12 +14,20 @@ define([
 
             constructor: function() {
                 this._basemaps = [
-                    {base: 'Antarctic Basemap', overlays: ['Reference'], label: 'Antarctic Basemap (IBCSO/GEBCO_08)'}
+                    {base: 'Antarctic Basemap', overlays: [{id: 'Reference'}], label: 'Antarctic Basemap (IBCSO/GEBCO_08)'}
                 ];
 
                 this._overlays = [
-                    {services: ['Reference'], label: 'Boundaries/Labels'},
-                    {services: ['Graticule'], label: 'Graticule'}
+                    {
+                        label: 'Boundaries/Labels',
+                        services: [{id: 'Reference'}],
+                        visible: false
+                    },
+                    {
+                        label: 'Graticule',
+                        services: [{id: 'Graticule'}],
+                        visible: true
+                    }
                 ];
 
                 //define the default base
--- historical_declination/js/app/web_mercator/MapToolbar.js
+++ historical_declination/js/app/web_mercator/MapToolbar.js
@@ -14,18 +14,26 @@ define([
 
             constructor: function() {
                 this._basemaps = [
-                    {base: 'Ocean Base', overlays: ['Ocean Reference'], label: 'Ocean Basemap (Esri)'},
-                    {base: 'GEBCO_08', overlays: ['World Boundaries and Places'], label: 'Shaded Relief (GEBCO_08)'},
-                    {base: 'ETOPO1', overlays: ['World Boundaries and Places'], label: 'Shaded Relief (ETOPO1)'},
-                    {base: 'Light Gray', overlays: ['Light Gray Reference'], label: 'Light Gray (Esri)'},
-                    {base: 'Dark Gray', overlays: ['Dark Gray Reference'], label: 'Dark Gray (Esri)'},
-                    {base: 'World Imagery', overlays: ['World Boundaries and Places'], label: 'World Imagery (Esri)'},
+                    {base: 'Ocean Base', overlays: [{id: 'Ocean Reference'}], label: 'Ocean Basemap (Esri)'},
+                    {base: 'GEBCO_08', overlays: [{id: 'World Boundaries and Places'}], label: 'Shaded Relief (GEBCO_08)'},
+                    {base: 'ETOPO1', overlays: [{id: 'World Boundaries and Places'}], label: 'Shaded Relief (ETOPO1)'},
+                    {base: 'Light Gray', overlays: [{id: 'Light Gray Reference'}], label: 'Light Gray (Esri)'},
+                    {base: 'Dark Gray', overlays: [{id: 'Dark Gray Reference'}], label: 'Dark Gray (Esri)'},
+                    {base: 'World Imagery', overlays: [{id: 'World Boundaries and Places'}], label: 'World Imagery (Esri)'},
                     {base: 'NatGeo', label: 'National Geographic (Esri)'} //NatGeo has no boundaries overlay
                 ];
 
                 this._overlays = [
-                    {services: ['Ocean Reference'], label: 'Boundaries/Labels'}, //Ocean Basemap/Reference is the default combo
-                    {services: ['Graticule'], label: 'Graticule'}
+                    {
+                        label: 'Boundaries/Labels',
+                        services: [{id: 'Ocean Reference'}],
+                        visible: false
+                    },
+                    {
+                        label: 'Graticule',
+                        services: [{id: 'Graticule'}],
+                        visible: false
+                    }
                 ];
 
                 //define the default base
