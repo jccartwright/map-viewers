d397a75d98f1f11ad1ce9f7969d6fb3295ef2337
jvarner <jesse.varner@noaa.gov>
2015-06-10 13:43:53 -0600

    geophysics: remove the "Draw Polygon" tool - not supported by NEXT.

--- geophysics/js/app/antarctic/MapToolbar.js
+++ geophysics/js/app/antarctic/MapToolbar.js
@@ -33,7 +33,6 @@ define([
                 this._identifyTools = [
                     {label: 'Point (Single-Click)', id: 'point', iconClass: 'identifyByPointIcon'},
                     {label: 'Draw Rectangle', id: 'rect', iconClass: 'identifyByRectIcon'},
-                    {label: 'Draw Polygon', id: 'polygon', iconClass: 'identifyByPolygonIcon'},
                     {label: 'Define Bounding Box', id: 'coords', iconClass: 'identifyByCoordsIcon'}
                 ];
 
