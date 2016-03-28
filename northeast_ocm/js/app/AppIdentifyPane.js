1099ab0621d0af1b2daf17a609663c732bfce294
John Cartwright <john.c.cartwright@noaa.gov>
2016-03-28 17:14:56 -0600

    northeast_ocm - switch from acceptance to production

--- northeast_ocm/js/app/AppIdentifyPane.js
+++ northeast_ocm/js/app/AppIdentifyPane.js
@@ -443,9 +443,7 @@ define([
             submitFormToNext: function(postBody) {
                 console.log("sending order via form submission to NEXT: ", postBody);
                 
-                //var url = "http://www.ngdc.noaa.gov/next-web/orders/create";
-                var url = "http://acceptance.ngdc.noaa.gov/next-web/orders/create";
-
+                var url = "http://www.ngdc.noaa.gov/next-web/orders/create";
 
                 //create a new form element and submit it.
                 var form = document.createElement("form");
