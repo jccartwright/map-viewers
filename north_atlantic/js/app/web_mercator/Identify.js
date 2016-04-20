dc86c387edd9bc40597e0753192ee7fa54b44035
John Cartwright <john.c.cartwright@noaa.gov>
2016-04-20 11:34:13 -0600

    north_atlantic: add EDMODnet layers as queryable

--- north_atlantic/js/app/web_mercator/Identify.js
+++ north_atlantic/js/app/web_mercator/Identify.js
@@ -22,7 +22,16 @@ define([
                 logger.debug('inside constructor for app/web_mercator/Identify');
 
                 //augment arguments object with list of layers to identify.
-                arguments[0].layerIds = ['Multibeam', 'Trackline Bathymetry', 'NRCan Multibeam', 'Portugal'];
+                arguments[0].layerIds = [
+                    'Multibeam',
+                    'Trackline Bathymetry',
+                    'NRCan Multibeam',
+                    'Portugal',
+                    'EMODNet Singlebeam Polygons',
+                    'EMODNet Multibeam Polygons',
+                    'EMODNet Singlebeam Lines',
+                    'EMODNet Multibeam Lines'
+                ];
 
                 //pass along reference to Map, LayerCollection, list of LayerIds
                 this.init(arguments);
