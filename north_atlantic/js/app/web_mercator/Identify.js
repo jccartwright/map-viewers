f3e703ed70e30d27ad3b2e89eba7b1465c3116e0
Jesse Varner <1390685153@mil>
2016-04-26 10:48:04 -0600

    north_atlantic: identify formatters setup.

--- north_atlantic/js/app/web_mercator/Identify.js
+++ north_atlantic/js/app/web_mercator/Identify.js
@@ -30,7 +30,9 @@ define([
                     'EMODNet Singlebeam Polygons',
                     'EMODNet Multibeam Polygons',
                     'EMODNet Singlebeam Lines',
-                    'EMODNet Multibeam Lines'
+                    'EMODNet Multibeam Lines', 
+                    'Protected Sites',
+                    'OER Planned Expeditions'
                 ];
 
                 //pass along reference to Map, LayerCollection, list of LayerIds
@@ -46,7 +48,14 @@ define([
                     'DEM Extents/All NCEI Bathymetric DEMs': lang.hitch(this, this.demFormatter),
                     'CSC Lidar/Lidar': lang.hitch(this, this.lidarFormatter),
                     'NRCan Multibeam/Multibeam Bathymetry Index Map - Bathymétrie Multifaisceaux Couches Index ': lang.hitch(this, this.nrCanFormatter),
-                    'Portugal/Proprietary Bathymetric Surveys (Portugal)': lang.hitch(this, this.portugalFormatter)
+                    'Portugal/Proprietary Bathymetric Surveys (Portugal)': lang.hitch(this, this.portugalFormatter),
+                    'EMODNet Multibeam Polygons/default': lang.hitch(this, this.emodnetMultibeamFormatter),
+                    'EMODNet Multibeam Lines/default': lang.hitch(this, this.emodnetMultibeamFormatter),
+                    'EMODNet Singlebeam Polygons/default': lang.hitch(this, this.emodnetSinglebeamFormatter),
+                    'EMODNet Singlebeam Lines/default': lang.hitch(this, this.emodnetSinglebeamFormatter),
+                    'Protected Sites/OSPAR MPA Network': lang.hitch(this, this.protectedSitesFormatter),
+                    'Protected Sites/NEAFC Closure Area': lang.hitch(this, this.protectedSitesFormatter),
+                    'OER Planned Expeditions/Okeanos Explorer 2016': lang.hitch(this, this.oerPlannedFormatter)
                 };
             } //end constructor
         });
