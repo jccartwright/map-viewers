a1f0f1c4f8c776ca1df223034f583060d5b48e69
Jesse Varner <1390685153@mil>
2016-04-27 14:03:42 -0600

    bathymetry: Added DEM Tiles. Replaced CSC with OCM everywhere.

--- bathymetry/js/app/LayersPanel.js
+++ bathymetry/js/app/LayersPanel.js
@@ -77,13 +77,14 @@ define([
 
                 on(this.chkDems, 'change', lang.hitch(this, function() {
                     topic.publish('/ngdc/layer/visibility', 'DEM Extents', this.chkDems.checked);
+                    topic.publish('/ngdc/layer/visibility', 'DEM Tiles', this.chkDems.checked);
                 })); 
                 on(this.chkDemHillshades, 'change', lang.hitch(this, function() {
                     topic.publish('/ngdc/layer/visibility', 'DEM Hillshades', this.chkDemHillshades.checked);
                 }));  
 
-                on(this.chkCscLidar, 'change', lang.hitch(this, function() {
-                    topic.publish('/ngdc/sublayer/visibility', 'CSC Lidar', [4], this.chkCscLidar.checked);                    
+                on(this.chkOcmLidar, 'change', lang.hitch(this, function() {
+                    topic.publish('/ngdc/sublayer/visibility', 'OCM Lidar', [4], this.chkOcmLidar.checked);                    
                 }));
 
                 on(this.searchButton, 'click', lang.hitch(this, function() {
