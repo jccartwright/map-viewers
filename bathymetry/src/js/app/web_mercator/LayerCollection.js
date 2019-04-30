ac5ed446ad661104cc17dd1d0676a83be4c11d0c
Jesse Varner <1390685153@mil>
2019-04-30 13:35:19 -0600

    bathymetry update to ArcGIS jsapi 3.28, dijits-2.13.1. Update to GEBCO_2019 basemaps/contours. Remove ETOPO1 basemap. rev to 2.16.0

--- bathymetry/src/js/app/web_mercator/LayerCollection.js
+++ bathymetry/src/js/app/web_mercator/LayerCollection.js
@@ -67,12 +67,12 @@ define([
                         id: 'NatGeo',
                         visible: false
                     }),
-                    new ArcGISTiledMapServiceLayer('https://tiles.arcgis.com/tiles/C8EMgrsFcRFL6LrL/arcgis/rest/services/GEBCO_2014_Global_Relief_Model_Color_Shaded_Relief/MapServer', {
-                        id: 'GEBCO_2014',
+                    new ArcGISTiledMapServiceLayer('https://tiles.arcgis.com/tiles/C8EMgrsFcRFL6LrL/arcgis/rest/services/GEBCO_2019_basemap_ncei/MapServer', {
+                        id: 'GEBCO_2019 (NCEI)',
                         visible: false
                     }),
-                    new ArcGISTiledMapServiceLayer('https://tiles.arcgis.com/tiles/C8EMgrsFcRFL6LrL/arcgis/rest/services/gebco_2014_hillshade_grayscale/MapServer', {
-                        id: 'GEBCO_2014 Grayscale',
+                    new ArcGISTiledMapServiceLayer('https://tiles.arcgis.com/tiles/C8EMgrsFcRFL6LrL/arcgis/rest/services/GEBCO_2019_grayscale_basemap_ncei/MapServer', {
+                        id: 'GEBCO_2019 Grayscale (NCEI)',
                         visible: false
                     }),
                     new ArcGISTiledMapServiceLayer('https://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer', {
@@ -125,11 +125,11 @@ define([
                         id: 'Multibeam Mosaic',
                         visible: false
                     }),
-                    new ArcGISTiledMapServiceLayer('https://tiles.arcgis.com/tiles/C8EMgrsFcRFL6LrL/arcgis/rest/services/gebco_2014_contours/MapServer', {
-                        id: 'GEBCO_2014 Contours',
+                    new ArcGISTiledMapServiceLayer('https://tiles.arcgis.com/tiles/C8EMgrsFcRFL6LrL/arcgis/rest/services/web_mercator_gebco_2019_contours/MapServer', {
+                        id: 'GEBCO_2019 Contours',
                         visible: false,
-                        opacity: 0.7
-                    }),                    
+                        opacity: 0.5
+                    }),                   
                     new ArcGISTiledMapServiceLayer('https://gis.ngdc.noaa.gov/arcgis/rest/services/bag_hillshades/ImageServer', {
                         id: 'BAG Hillshades (tiled)',
                         visible: false
